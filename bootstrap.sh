21364dc4f90698d898145f101b1752f17d5b6a10
Lachie Cox <lachiec@gmail.com>
2012-12-24 14:37:35 +1100

    tmp chdir to vendor

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,10 +17,12 @@ cd $WORKDIR
 JQ=./vendor/jq
 S3CURL=./vendor/s3curl.pl
 
-curl -LO $BOOTSTRAP_HOME/vendor/jq
-chmod 0755 $JQ
+( cd vendor
+  curl -LO $BOOTSTRAP_HOME/vendor/jq
+  curl -LO $BOOTSTRAP_HOME/vendor/s3curl.pl
+)
 
-curl -LO $BOOTSTRAP_HOME/vendor/s3curl.pl
+chmod 0755 $JQ
 chmod 0755 $S3CURL
 
 
